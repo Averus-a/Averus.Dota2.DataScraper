b9468001ee78e0221efccb9d258d1b76313d351a
Artem Saikin <art.saikin@gmail.com>
2023-07-07 00:40:08 +0300

    Add project files.

---
+++ Averus.Dota2.DataScraper/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Averus.Dota2.DataScraper
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello! This is DOTA 2 data scraper build as tool for working on WoF AI bot script.");
+
+
+        }
+    }
+}
